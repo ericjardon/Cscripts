1bebb458e08e6dff68ea5827a2f5f2d7bc982897
Eric Chao <ericjardon@hotmail.com>
2021-05-23 18:12:14 -0500

    Split file into chunks

--- MapReduceIndex/main.c
+++ MapReduceIndex/main.c
@@ -5,7 +5,9 @@
 #include <unistd.h>
 #include <pthread.h>
 #include<sys/wait.h>
+#include <sys/types.h>
 #include "header.h"
+
 
 #define PRUEBA "prueba.txt"
 #define ALICE "alice.txt"
@@ -16,82 +18,75 @@
 //3. Desde un programa, recorremos cada uno de los archivos invertidos (A1 y A2 en conjunto) de los mappers, se leen secuencialmente para generar un arreglo ordenado de structs de {palabra, [ocurrencias]}
 // 4. ordenamos el arreglo con quicksort, lo leemos para reducirlo y escupimos A1 y A2 reducidos final.
 
+typedef struct line_record {
+    char line[100];
+    int number;
+} line_record;
+
+// Local function declarations
+int printChunk(FILE*);
+
 
 // Function to split input file into 4 chunks
-int divideFile(FILE* file_ptr) {
+int splitFile(FILE* file_ptr) {
   FILE *ptr1, *ptr2, *ptr3, *ptr4;
   
   ptr1 = fopen("chunk1.txt", "w+");
   ptr2 = fopen("chunk2.txt", "w+");
   ptr3 = fopen("chunk3.txt", "w+");
   ptr4 = fopen("chunk4.txt", "w+");
-
   char* line;
   size_t line_size = sizeof(char) * 100; // bytes
   line = (char * ) malloc(line_size);
 
+  int counter = 0;
   int flag = 0;
-  const char sep[2] = " ";
 
   // Read every line
   while(getline(&line, &line_size, file_ptr) != -1) {
-    char *token;
-    token = strtok(line, sep); // tokenize the line
+    counter++;
+    line_record* r = (line_record *) malloc(sizeof(line_record));  
+    strcpy(r->line, line);
+    r->number = counter;
 
-    // Copy the token into word, removing non-alphabetic
-    while (token != NULL) {
-      char word[50]; //
-      memset(word,'\0',sizeof(word));
-
-      int j = 0; // index for clean word
-
-      for (int i=0;token[i]!='\0'; i++) {
-        if(token[i]>='A' && token[i]<='Z') {
-          token[i] += 32; // to lowercase
-        }
-        if(token[i]>='a' && token[i]<='z') {
-          word[j] = token[i];
-          j++;
-        } else if (j>=1) break; // end of word
-      }
-
-      // If word is non-empty
-      if (strcmp("", word) != 0) {
-        
+    if (strcmp("", r->line) != 0) {
+        printf("Write to chunk %d", flag);
         if (flag ==0) {
           // Write string to A
-          fprintf(ptr1, "%s\n", word);
+          fprintf(ptr1, "%d ", r->number);
+          fprintf(ptr1, "%s", r->line);
           flag = 1;
+          continue;
         }
-
         else if (flag==1) {
           // Write string to B
-          fprintf(ptr2, "%s\n", word);
+          fprintf(ptr2, "%d ", r->number);
+          fprintf(ptr2, "%s", r->line);
           flag = 2;
+          continue;
         } 
 
         else if (flag==2) {
           // Write string to C
-          fprintf(ptr3, "%s\n", word);
+          fprintf(ptr3, "%d ", r->number);
+          fprintf(ptr3, "%s", r->line);
           flag = 3;
+          continue;
         } 
         else if (flag==3) {
           // Write string to D
-          fprintf(ptr4, "%s\n", word);
+          fprintf(ptr4, "%d ", r->number);
+          fprintf(ptr4, "%s", r->line);
           flag = 0;
+          continue;
         }
       }
- 
-      token = strtok(NULL, sep); // next token
-    } // end of while per token
-
   } // end of while per line
   fclose(ptr1);
   fclose(ptr2);
   fclose(ptr3);
   fclose(ptr4);
   fclose(file_ptr);
-  
   return 0;
 }
 
@@ -100,7 +95,7 @@ int main(int argc, char* argv[]) {
   FILE* file_ptr;
   file_ptr = fopen(PRUEBA, "r");
 
-  divideFile(file_ptr); 
+  splitFile(file_ptr); 
 
   int n = NUM_PROC;
   printf("Typed n:%d\n", n);
@@ -109,29 +104,48 @@ int main(int argc, char* argv[]) {
   int status = 0;
 
   //Father code (before child processes start)
-  printf("Padre PID: %d\n", getpid());
+  //printf("Padre PID: %d\n", getpid());
 
-  FILE* pointers[NUM_PROC];
+/*   FILE* pointers[NUM_PROC];
   pointers[0] = fopen("chunk1.txt", "r");
   pointers[1] = fopen("chunk2.txt", "r");
   pointers[2] = fopen("chunk3.txt", "r");
   pointers[3] = fopen("chunk4.txt", "r");
 
+  // fork 4 processes
   for (int id=0; id<NUM_PROC; id++) {
       if ((child_pid = fork()) == 0) {
           printf("Hijo %d PID: %d\n", id, getpid());
 
-          chunkMap(id, pointers[id]);
-
+          chunkMap(id, pointers[id]); // produce A1 and A2 for each chunk
           fclose(pointers[id]);
           exit(0);
       }
-  }
+  } */
 
-  while ((wpid = wait(&status)) > 0);
-  printf("All done\n");
+  // Wait for all processes
+  //while ((wpid = wait(&status)) > 0);
   return 0;
 }
+
+/* IMPRIMIR ARCHIVOS */
+int printChunk(FILE* ar) {
+  printf("printChunk started\n");
+  int i=0;
+  
+  while (fseek(ar, sizeof(line_record)*i, SEEK_SET) == 0){
+    line_record lr;
+    fread(&lr, sizeof(line_record), 1, ar);
+    if (feof(ar)) { // End of File
+    printf("FINISHED FILE\n");
+      break;
+    }
+    printf("%d: >>%s<<\n" , lr.number,lr.line);
+    i++;
+  }
+  return 0;
+}
+
 
   // Separate the file into four different parts
   // Do two consecutive forks, to create 4 processes
