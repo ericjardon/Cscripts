b22867278c616abee355af2d86590be064de9f81
Eric Chao <ericjardon@hotmail.com>
2021-05-23 19:10:27 -0500

    single chunk inverted index, no testing yet

--- MapReduceIndex/mapper.c
+++ MapReduceIndex/mapper.c
@@ -45,15 +45,209 @@ void freeArray(Array *a) {
   a->used = a->size = 0;
 }
 
-void chunkMap(int id, FILE* chunk) {
+int printFile1(FILE * a1, int id);
+int printFile2(FILE * a2, int id);
 
-    printf("chunkMap called by %d\n", id);
-    char sid[2];
-    sprintf(sid, "%d", id);
+int compareUser(const void *v1, const void *v2)
+{
+  // comparación para ordenamiento de registros 1
+  const struct record1* r1 = v1;
+  const struct record1* r2 = v2;
+  return strcmp(r1->word, r2->word);
+}
+
+
+int traverseArchivo1(FILE* ar, char* target) {
+  int i = 0;
+  while (fseek(ar, sizeof(record1)*i, SEEK_SET) == 0){
+    record1 r1;
+    fread(&r1, sizeof(record1), 1, ar);
+    if(strcmp(r1.word, target)==0){
+      return i;
+    }
+    if (feof(ar)) {
+      // Indica si tratamos de escribir más allá de EOF
+      break;
+    }
+    i++;
+  }
+  return -1;  // not found
+}
+
+
+void chunkMap(int id, FILE* chunk) {
+  char sid[2];
+  sprintf(sid, "%d", id+1);   // int -> string
+  
+  char chunk_txt[11] = "chunk"; // chunkX.txt
+  strcat(chunk_txt, sid); strcat(chunk_txt, ".txt");
+
+  printf("To read: %s\n", chunk_txt);
+
+  FILE* toread;
+  toread = fopen(chunk_txt, "r");
+
+  char name1[4] = A1; // A1X
+  char name2[4] = A2; // A2X
+
+  strcat(name1, sid);
+  strcat(name2, sid);
+
+  FILE* archivo1 = fopen(name1, "wb+");
+  FILE* archivo2 = fopen(name2, "wb+");
+  // START PROCESSING
+
+  int posA2 = 0;
+  Array a;
+  initArray(&a, 50);  // initially 50 elements
+  
+  // Lectura línea por línea
+  char* line;
+  size_t size = sizeof(char)*100;
+  line = (char *) malloc(size);
+  int line_index; // first token of a line
+
+  while( getline(&line, &size, toread) != -1 ) {
+    const char s[2] = " ";
+    char *token;
+    // First token is line number
+    token = strtok(line, s);
+    line_index = atoi(token);
+
+    token = strtok(NULL, s); // Start word by word
+    while( token != NULL ) {
+      char clean[50];
+      int j = 0;
+      for (int i=0; token[i]!='\0'; i++) {
+          // convertir mayusculas a minusculas
+          if(token[i]>='A' && token[i]<='Z'){
+            token[i] = token[i]+32;
+          }
+          // si es una letra, copiala a clean[j]
+          if(token[i]>= 'a' && token[i]<='z'){
+            clean[j]=token[i];
+            j++;
+            continue;
+          } else {    // if next is non-alpahebtic and we've at least 1 char, stop
+            if (j>=1) break;
+          }
+      }
     
-    char name1[4] = A1;
-    strcat(name1, sid);
-    char name2[4] = A2;
-    strcat(name2, sid);
-    
+      if (strcmp("", clean)!=0) {
+        int positionA1  = traverseArchivo1(archivo1, clean); 
+        if (positionA1 == -1) {
+          // if string not empty, write to archivo1
+          record1 r1;
+          strcpy(r1.word, clean);
+          r1.ptr = posA2;  
+          fwrite(&r1, sizeof(record1), 1, archivo1);
+          insertArray(&a, r1);
+          // TO CREATE archivo2 ENTRIES:
+          // 1. Take current line (line_index)
+          // 2. take current position counter posA2
+          // 3. write into archivo2 starting from posA2 <line_index,-1>
+          record2 r2;
+          r2.line = line_index;
+          r2.next = -1;
+          fseek(archivo2, sizeof(record2)*posA2, SEEK_SET);
+          fwrite(&r2, sizeof(record2), 1, archivo2);
+          
+          // 4. increment posA2.
+          posA2++;
+
+        } else { // Duplicate word at positionA1 in archivo1
+            
+            // Access entry at positionA1 in archivo 1
+            fseek(archivo1, sizeof(record1)*positionA1, SEEK_SET); 
+            record1 r1;     
+            fread(&r1, sizeof(record1), 1, archivo1);
+            // Move to its pointer in A2
+            fseek(archivo2, sizeof(record2)*r1.ptr, SEEK_SET);
+            record2 r2;
+            fread(&r2, sizeof(record2), 1, archivo2); // read in record r2
+
+            // traverse linked list in A2
+            int prevR2 = r1.ptr;      
+            while (r2.next != -1) {   
+              prevR2 = r2.next;  // last valid pointer in R2
+              //printf("Lista invertida next: %d", r2.next);
+              fseek(archivo2, sizeof(record2)*r2.next, SEEK_SET);
+              fread(&r2, sizeof(record2), 1, archivo2); // reasignas r2
+            } // stop when r2.next == -1;
+            
+            r2.next = posA2; // update `next` pointer in A2
+            fseek(archivo2, sizeof(record2)*prevR2, SEEK_SET);
+            fwrite(&r2, sizeof(record2), 1, archivo2);
+            // Add new r2 entry
+            record2 newR2;
+            newR2.line = line_index;
+            newR2.next = -1;
+            fseek(archivo2, sizeof(record2)*posA2, SEEK_SET);
+            fwrite(&newR2, sizeof(record2), 1, archivo2);
+            posA2++;
+        }
+      }
+     
+      memset(clean,'\0',sizeof(clean));   // reset `clean`
+      token = strtok(NULL, s); // next token
+
+    } // end while for every line
+
+  }
+
+  // Sort record1 array
+  qsort(a.array, a.used, sizeof(record1), compareUser);
+  fseek(archivo1, 0, SEEK_SET);
+  
+  // write into A1
+  for(int i=0; i<a.used; i++) {
+    fwrite(&(a.array[i]), sizeof(record1), 1, archivo1);
+  }
+
+  // print files before closing to check
+  if (id==0) {
+    printFile1(archivo1, 0);
+    putchar('\n');
+    printFile2(archivo2, 0);
+  }
+
+
+  // END PROCESSING
+  fclose(archivo1);
+  fclose(archivo2);
+  freeArray(&a);
+  fclose(toread);
+
+}
+
+
+int printFile1(FILE * a1, int id) {
+  int i=0;
+  
+  while (fseek(a1, sizeof(record1)*i, SEEK_SET) == 0){
+    record1 r1;
+    fread(&r1, sizeof(record1), 1, a1);
+    if (feof(a1)) {
+      // Indica si tratamos de escribir más allá de EOF
+      break;
+    }
+    printf("R1 Word:%s\n R1 ptr: %d\n",r1.word, r1.ptr);
+    i++;
+  }
+  return 0;
+}
+
+int printFile2(FILE* a2, int id) {
+  int i=0;
+  while (fseek(a2, sizeof(record2)*i, SEEK_SET) == 0){
+    record2 r2;
+    fread(&r2, sizeof(record2), 1, a2);
+    if (feof(a2)) {
+      // Indica si tratamos de escribir más allá de EOF
+      break;
+    }
+    printf("R2 %d\t Line occurence: %d\n R2 next: %d\n",i, r2.line, r2.next);
+    i++;
+  }
+  return 0;
 }
